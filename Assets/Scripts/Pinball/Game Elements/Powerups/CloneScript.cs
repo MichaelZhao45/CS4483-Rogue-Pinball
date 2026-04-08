4bf2c60bfe171ebdeca98f8f8f47620d8e3ce3a1
sbugti <sbugti04@gmail.com>
2026-04-07 22:07:38 -0400

    Clone powerup done(?) and shop gameobject added

--- Assets/Scripts/Pinball/Game Elements/Powerups/CloneScript.cs
+++ Assets/Scripts/Pinball/Game Elements/Powerups/CloneScript.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class CloneScript : PowerUp
 {
+    [SerializeField] public GameController gameController;
+    [SerializeField] public GameObject ball;
+    [SerializeField] public BallManager ballManager;
+
     public override void OnUse()
     {
-        //inventoryManager should call this function upon being consumed
-        //if pinball map is being used, and game is currently being played:
-        //create a second pinball at the current pinballs location, and send both balls in opposite horizontal directions
-        //then delete the powerup object
+        Vector3 position = ball.transform.position;
+        ballManager.SpawnBall(position);
+        Destroy(gameObject);
     }
 }
