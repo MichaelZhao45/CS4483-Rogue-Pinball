7396308d1b6dfd92abf4f1cf24b84e2a8bac9a7b
Liam Elliott <liam24elliott@gmail.com>
2026-04-14 16:00:04 -0400

    Altered exit door behaviour
    - Changed the exit door to require an intentional 'E' button press instead of simply walking into it.
    - The exit sequence plays a door opening sound and has a fade-to-black, for cinematic effect.

--- Assets/Scripts/Hub World/Interaction/ExitDoorInteraction.cs
+++ Assets/Scripts/Hub World/Interaction/ExitDoorInteraction.cs
@@ -1,13 +1,32 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class ExitDoorInteraction : InteractionBase
 {
-    protected override void OnTriggerEnter(Collider other)
+    public Image fadeCover;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
     {
-        if (other.CompareTag("Player"))
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (player != null && playerNearby)
         {
-            Debug.Log("Game quit");
-            Application.Quit();
+            if (context.performed)
+            {
+                Sequence fadeSequence = DOTween.Sequence();
+                fadeSequence.AppendCallback(_audioSource.Play);
+                fadeSequence.Append(fadeCover.DOFade(1f, 2f));
+                fadeSequence.AppendCallback(Application.Quit);
+                fadeSequence.Play();
+            }
         }
     }
 }
